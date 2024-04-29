1b71196aeae9dcd972dd597fffafbd8c28b3a88b
Ivan Palamarchuk <ipz222_pis@student.ztu.edu.ua>
2024-04-29 22:54:42 +0300

    lab5: command

---
+++ lab-5/CommandDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using LightHTML;
+using LightHTML.command;
+
+LightElementNode node = new("div", "block-level", true);
+LightTextNode textNode = new("text");
+List<ICommand> commands =
+[
+    new NodeCommand(() => { node.AddNode(textNode); }, () => { node.RemoveNode(textNode); }),
+    new NodeCommand(() => { node.CSSClasses.Add("container"); }, () => { node.CSSClasses.Remove("container"); }),
+];
+
+Invoker invoker = new(commands);
+
+invoker.Run();
+PrintInfo(node);
+
+invoker.Revert();
+PrintInfo(node);
+
+static void PrintInfo(LightElementNode node)
+{
+    Console.WriteLine("classes: " + string.Join(" ", node.CSSClasses));
+    Console.WriteLine("children (amount): " + node.GetNodes().Count);
+}
